76c577e87664ba18b5b134fe45b88f8bfa1f942a
TaylorChang7 <hct9at@virginia.edu>
2020-06-22 13:00:18 -0500

    Updated version (Note that this is not final draft yet)

--- components/Landing/subpages/Facilities.js
+++ components/Landing/subpages/Facilities.js
@@ -1,11 +1,304 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image,Linking} from 'react-native';
 
 export default function Facilities(props) {
 
     return (
         <View>
-            <Text>Facilities</Text>
+          <View style = {styles.toCenter}>
+            <Text style = {styles.textType}>
+              <Text style = {styles.textColor}>
+                  <Text style = {styles.titleText}> Facilities </Text>
+               </Text>
+            </Text>
+            <Image
+              style = {styles.facilitiesPic}
+              source = {{uri: 'https://lase.mer.utexas.edu/images/facilities_large.jpg' }}
+            />
+            <Text> {"\n"} </Text>
+          </View>
+
+            <Text
+            style = {styles.sectionText}>{'\t'}“Bravo” Varian Gen II MBE (Status: Operational)
+            </Text>
+
+              <Text> {'\t'}{'\t'}History: Donation from Bell Labs, installed 1994 and refurbished 2007</Text>
+              <Text> {'\t'}{'\t'}Materials:</Text>
+
+                <Text>
+                  {'\t'}{'\t'}{'\t'}{'\t'}Er – Veeco 10CC high temperature source{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Ga – Veeco 400g SUMO cell{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}In – Veeco 900g SUMO cell{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Al – Veeco 200g SUMO cell{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}N – SVTA rf plasma source (Status: not installed){'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}As – Veeco 500CC valved-cracker{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Sb – Veeco 200CC valved-cracker{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Bi – Veeco 250g downward-looking SUMO cell{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Si/Be – MBE Komponenten custom dual dopant source (Si filament and Be effusion){'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}GaTe – MBE Komponenten tilted crucible dopant source{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Si – MBE Komponenten filament source{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Be – MBE Komponenten tilted crucible dopant source{'\n'}
+                </Text>
+
+              <Text> {'\t'}{'\t'}Ancillary Equipment:</Text>
+                <Text>
+                  {'\t'}{'\t'}{'\t'}{'\t'}kSA multibeam optical stress (MOS) system{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}kSA BandiT bandedge/blackbody temperature measurement{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Staib 15 keV RHEED system{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}kSA RHEED analysis{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Stanford Research Systems RGA 300{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Veeco externally adjustable group-III shutters{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Riber (formerly MBE Control) AMBER growth software{'\n'}
+                  {'\t'}{'\t'}{'\t'}{'\t'}Veeco 3” substrate manipulator{'\n'}
+                </Text>
+
+          <Text
+            style = {styles.sectionText}>{'\t'}“Echo” EPI MOD Gen II MBE (Status: Operational)
+          </Text>
+
+            <Text> {'\t'}{'\t'}History: Purchased in 1995 by NIST, donated to LASE in 2008, refurbished in 2009</Text>
+            <Text> {'\t'}{'\t'}Materials:</Text>
+              <Text>
+                {'\t'}{'\t'}{'\t'}{'\t'}B - MBE Komponenten EBVV vertical e-beam evaporator{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Ga – Veeco 400g SUMO cell{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}In – Veeco 400g SUMO cell{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}P – E-SCIENCE valved GaP decomposition source{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}As – Veeco 500CC valved-cracker{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Bi – Veeco 250g downward-looking SUMO cell{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Si/Be – Veeco dual dopant source{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}GaTe/Er - Veeco dual dopant source{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}La – Veeco 10CC high temperature source (Status: not installed){'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Lu – Veeco 10CC high temperature source (Status: not installed){'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}C – Veeco CBr4 with MBE Control custom injector (Status: not installed){'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Al – Veeco 200g SUMO cell (Status: not installed){'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Gd – Veeco 10CC high temperature source (Status: not installed){'\n'}
+              </Text>
+
+            <Text> {'\t'}{'\t'}Ancillary Equipment:</Text>
+              <Text>
+                {'\t'}{'\t'}{'\t'}{'\t'}kSA BandiT bandedge/blackbody temperature measurement (under installation){'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}MBE Control EZ-RHEED analysis{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Stanford Research Systems RGA 200{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Veeco externally adjustable group-III shutters{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Riber (formerly MBE Control) AMBER growth software{'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Custom cryogen-free P-recovery system (Status: under installation){'\n'}
+                {'\t'}{'\t'}{'\t'}{'\t'}Veeco 3” substrate manipulator{'\n'}
+              </Text>
+        <Text
+          style = {styles.sectionText}>{'\t'}“Foxtrot” (a.k.a. “The Juice”) Varian Chamber (Status: Under installation)
+        </Text>
+
+          <Text> {'\t'}{'\t'}History: Donation from UT-Arlington in 2011</Text>
+          <Text> {'\t'}{'\t'}Materials:</Text>
+            <Text>
+              {'\t'}{'\t'}{'\t'}{'\t'}Epitaxial plasmonic and emerging materials{'\n'}
+            </Text>
+
+          <Text> {'\t'}{'\t'}Ancillary Equipment:</Text>
+            <Text>
+              {'\t'}{'\t'}{'\t'}{'\t'}Thermionics 6-pocket e-beam evaporator{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Veeco dual dopant source{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}MBE Komponenten cooling water nipple and shutter{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Stanford Research Systems RGA 200{'\n'}
+            </Text>
+
+            {
+              /*
+              <Text   style={styles.link}
+                      onPress={() => Linking.openURL("http://www.mrc.utexas.edu/")}>Microelectronics Research Center</Text>
+              */
+            }
+
+
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Bakeout Structure ( <Text style = {styles.link} onPress= {() => Linking.openURL("https://lase.mer.utexas.edu/images/BOS_v2.jpg")}>Bos </Text>) (Status: Operational, v2.1)
+        </Text>
+
+          <Text> {'\t'}{'\t'}Outgassing MBE components prior to introducing them to the MBE system</Text>
+          <Text> {'\t'}{'\t'}Equipment:</Text>
+            <Text>
+              {'\t'}{'\t'}{'\t'}{'\t'}CTI CT10 cryo{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Stanford Research Systems RGA 200{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Four 8" arms for baking sources{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}One 10" arm for baking substrate manipulator{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Vertical group-III thermal deposition system (indium currently installed){'\n'}
+            </Text>
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Optical characterization setups
+        </Text>
+          <Text>
+            {'\t'}{'\t'}Photoluminescence (PL): Measuring luminescence efficiency, emission wavelength, and so on{'\n'}
+            {'\t'}{'\t'}near/mid-IR Reflection/Transmission (R&T): Measuring absorption and band-edge{'\n'}
+            {'\t'}{'\t'}Pump/probe: Femtosecond carrier dynamics using mode-locked fiber laser{'\n'}
+            {'\t'}{'\t'}Photoreflectance (PR): Measuring band alignments{'\n'}
+          </Text>
+          <Text> {'\t'}{'\t'}Ancillary Equipment:</Text>
+            <Text> {'\t'}{'\t'}{'\t'}{'\t'}LHe cryostat and controller{'\n'}</Text>
+
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Fourier transform infrared spectrometer (FTIR) and IR microscope (<Text style = {styles.link} onPress= {() => Linking.openURL("https://lase.mer.utexas.edu/images/FTIR.jpg")}>Futur-er</Text>)
+        </Text>
+          <Text>
+              {'\t'}{'\t'}FTIR: Bruker Vertex v80
+          </Text>
+            <Text>
+              {/*
+                Need to do this
+              */}
+              {'\t'}{'\t'}{'\t'}{'\t'}Operating range: 5 - 25000 cm-1 (0.4 – 2000 μm){'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Step scan{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Rapid scan{'\n'}
+            </Text>
+          <Text>
+              {'\t'}{'\t'}IR Microscope: Bruker Hyperion 2000
+          </Text>
+            <Text>
+              {'\t'}{'\t'}{'\t'}{'\t'}Coupled to FTIR for spatial/spectral/temporal mapping{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Single point MCT detector w/automated stage for imaging{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}ZnSe A ttenuated Total Reflectance (ATR) objective (ZnSe){'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Grazing Angle Objective (GAO){'\n'}
+            </Text>
+          <Text>
+              {'\t'}{'\t'}Accessories:
+          </Text>
+            <Text>
+              {'\t'}{'\t'}{'\t'}{'\t'}Keysight B1500A semiconductor device analyzer (1x high-power, 2x high-resolution SMUs){'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}MMR variable temperature micro miniature refrigerator w/controller (under installation){'\n'}
+            </Text>
+
+
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Edge-emitting laser (EEL) test setup
+        </Text>
+          <Text>
+              {'\t'}{'\t'}CW, pulsed, and temperature-dependant EEL characterization
+          </Text>
+          <Text>
+              {'\t'}{'\t'}Equipment:
+          </Text>
+            <Text>
+              {'\t'}{'\t'}{'\t'}{'\t'}Vigo high-speed (ns) MCT detector (5μm cutoff){'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Thorlabs InGaAs amplified detector (1.5μm cutoff){'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Princeton Instruments Acton 2500 spectrometer{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Labsphere Infragold 2" integrating sphere with fiber port{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}ILX Laser drivers - pulsed (LDP-3840B), CW/temperature (LDC-3744){'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}ILX temperature-controlled laser mount (LDM-4415){'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Neslab recirculating chiller{'\n'}
+            </Text>
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Photodetector / Photocurrent spectroscopy setup (visible-to-mid-IR)
+        </Text>
+          <Text>
+              {'\t'}{'\t'}Spectrally-resolved photocurrent/photovoltage response
+          </Text>
+          <Text>
+              {'\t'}{'\t'}Equipment:
+          </Text>
+
+            <Text>
+              {'\t'}{'\t'}{'\t'}{'\t'}Stanford research systems lock-in amplifier and chopper{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Princeton Instruments Acton 2500 spectrometer{'\n'}
+              {'\t'}{'\t'}{'\t'}{'\t'}Various light sources{'\n'}
+            </Text>
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Probe station
+        </Text>
+          <Text>
+              {'\t'}{'\t'}I-V: Tunnel junctions characterization and process flow diagnostics (e.g. TLM)
+          </Text>
+          <Text>
+              {'\t'}{'\t'}Equipment:
+          </Text>
+            <Text>
+            {'\t'}{'\t'}{'\t'}{'\t'}Micromanipulator 6200 probe station{'\n'}
+            {'\t'}{'\t'}{'\t'}{'\t'}Keysight B1500A semiconductor device analyzer (1x high-power, 2x high-resolution SMUs){'\n'}
+            {'\t'}{'\t'}{'\t'}{'\t'}HP 4145 semiconductor parameter analyzer{'\n'}
+            </Text>
+
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Simulations
+        </Text>
+          <Text>
+              {'\t'}{'\t'}VASP (TACC) - Density Functional Theory (DFT){'\n'}
+              {'\t'}{'\t'}nextnano (Windows) - k.p, wavefunction solver, electrostatics, etc.{'\n'}
+              {'\t'}{'\t'}RCWA (Windows) - Rigorous coupled-wave analysis; tool developed by <Text style = {styles.link} onPress= {() => Linking.openURL("http://faculty.uml.edu/vpodolskiy/codes/index.html")}>Podolskiy Group, UML</Text>{'\n'}
+              {'\t'}{'\t'}COMSOL (Windows) - Finite element method (FEM){'\n'}
+              {'\t'}{'\t'}Lumerical (Windows) - Finite-difference time-domain (FDTD) and FEM{'\n'}
+              {'\t'}{'\t'}MEEP (Unix) - FDTD{'\n'}
+              {'\t'}{'\t'}Monte Carlo (Unix) - APD simulations, including noise; tool developed by <Text style = {styles.link} onPress= {() => Linking.openURL("http://www.ece.virginia.edu/pdg/index.html")}>Campbell Group, UVA</Text>{'\n'}
+              {'\t'}{'\t'}Band diagramming (Java and Python) - Poisson and Poisson-Schrodinger solvers, e.g. <Text style = {styles.link} onPress= {() => Linking.openURL("https://github.com/scott-maddox/openbandparams")}>OpenBandParams</Text>{'\n'}
+            </Text>
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Other equipment available through Microelectronics Research Center (NSF-NNCI)
+        </Text>
+
+          <Text>
+            {'\t'}{'\t'}Processing: III-V and Si processing (litho, ICP/RIE dry etch, metallization, etc), wafer bonding, {'\n'}
+            {'\t'}{'\t'}lapping/polishing, wire bonding, etc.{'\n'}
+            {'\t'}{'\t'}Characterization: HR-XRD (rotating anode Rigaku SmartLab), AFM, TEM, Hall Effect, PPMS, C-V, etc.{'\n'}
+          </Text>
+
+        <Text
+          style = {styles.sectionText}>{'\t'}Advanced energetics
+        </Text>
+
+          <Text>
+            {'\t'}{'\t'}Espresso - Illy Francis Francis! Model X7.1 IperEspresso Machine{'\n'}
+            {'\t'}{'\t'}Pourover (6 cup) - Chemex Classic Series{'\n'}
+            {'\t'}{'\t'}French Press (20 oz.) - Bodum Chambord (all-metal construction){'\n'}
+            {'\t'}{'\t'}Jura-Capresso Infinity (commercial-grade conical burr grinder){'\n'}
+            {'\t'}{'\t'}Dairy - Nespresso Aerocinno Plus{'\n'}
+          </Text>
+
+
+
+
         </View>
+
     );
+
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  toCenter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  textType: {
+    fontFamily: "Coching"
+  },
+  facilitiesPic: {
+    width: 550,
+    height: 400,
+  },
+  BoldText: {
+    fontWeight: 'bold'
+  },
+  textColor: {
+      color: 'orange'
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  sectionText: {
+    fontSize: 15,
+    fontWeight: "bold"
+  },
+  link: {color: "#c60"},
+
+
+});
