7ebac5d9e8da515dfc30e75b87681c6d68cb42a0
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-06-30 15:58:11 -0400

    Basic filtering support for Maintenance record

--- components/Maintenance/Find/Filter.js
+++ components/Maintenance/Find/Filter.js
@@ -2,11 +2,12 @@
 //  abbreviated list form; allow the user to select and view a second record.
 
 // Imports
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, FlatList, ScrollView, ActivityIndicator } from 'react-native';
 import { BASE_URL } from '../../../constants/API.js';
-import { Jet, Platinum, Gainsboro, InternationalOrange, EgyptianBlue } from '../../../constants/Colors.js';
 import { Ionicons } from '@expo/vector-icons';
+import SelectSystem from '../../lib/forms/SelectSystem';
+import SelectMember from '../../lib/forms/SelectMember';
 
 // Helper method which retrieves all the maintenance records from the API. It's not
 //  just as straightforward as a single API call, though, since the results are paginated
@@ -25,11 +26,21 @@ const GetAllRecords = async () => {
     return records;
 }
 
+// Function used to filter the displayed items based on the applied constraints
+const FilterFx = filter => {
+    return record => {
+        for(var key in filter) if(filter[key] !== "" && record[key] !== filter[key]) return false;
+        return true;
+    };
+};
+
 // The strucutre of this class, excluding the tools to filter, is more or less
-//  technically identical to the portion of the Overview subpage in the Landing
+//  technfilterically identical to the portion of the Overview subpage in the Landing
 //  folder which deals with retrieving the list of recent publications.
 export default function Filter(props) {
     let [records, setRecords] = useState({loaded: false, items: []});
+    let [filter, setFilter] = useState({});
+    let [[system, setSystem], [recorder, setRecorder], [p1, setP1], [p2, setP2]] = [useState(""), useState(""), useState(""), useState("")];
 
     useEffect(() => {
         let load = async () => {
@@ -39,36 +50,52 @@ export default function Filter(props) {
         load();
     }, [0]);
 
+    // Refilters records every time the filter is changed
+    useEffect(() => {
+        setFilter({
+            system,
+            recorder,
+            p1,
+            p2,
+        })
+    }, [system, recorder, p1, p2]);
+
     return (
         <View style={styles.container}>
             {
                 records.loaded ? (
                     <View style={styles.listContainer}>
-                        <ScrollView>
-                            {/* FlatLists are easily one of the most power components in React. Learning how to make good use of them is a must! */}
-                            <FlatList   style={styles.list}
-                                        data={records.items}
-                                        keyExtractor={item => item.id.toString()}
-                                        renderItem={({item}) => (
-                                            <View   style={styles.recordRow}>
-                                                <View>
-                                                    <TouchableOpacity   style={styles.openRecordButton}
-                                                                        onPress={() => props.navigation.navigate("Record", {record: item})}>
-                                                        <Ionicons name="md-open" size={16} color={EgyptianBlue} style={{position: "relative", left: 3, top: 1}}/>
-                                                    </TouchableOpacity>
-                                                </View>
-                                                <View style={{width: 60}}>
-                                                    <Text style={styles.rowText}>{item.system}</Text>
-                                                </View>
-                                                <View style={{width: 75}}>
-                                                    <Text style={styles.rowText}>{item.date}</Text>
-                                                </View>
-                                                <View>
-                                                    <Text style={styles.rowText}>{item.summary}</Text>
-                                                </View>
+                        <View style={styles.filterControls}>
+                            <SelectSystem placeholder={{label: "Select System", value: ""}} update={setSystem}/>
+                            <SelectMember placeholder={{label: "Select Recorder", value: ""}} update={setRecorder}/>
+                            <SelectMember placeholder={{label: "Select P1", value: ""}} update={setP1}/>
+                            <SelectMember placeholder={{label: "Select P2", value: ""}} update={setP2}/>
+                            <Text>Filter records</Text>
+
+                        </View>
+                        {/* FlatLists are easily one of the most power components in React. Learning how to make good use of them is a must! */}
+                        <FlatList   style={styles.list}
+                                    data={records.items.filter(FilterFx(filter))}
+                                    keyExtractor={item => item.id.toString()}
+                                    renderItem={({item}) => (
+                                        <View style={styles.recordRow}>
+                                            <View>
+                                                <TouchableOpacity   style={styles.openRecordButton}
+                                                                    onPress={() => props.navigation.navigate("Record", {record: item})}>
+                                                    <Ionicons name="md-open" size={16} color="blue" style={{position: "relative", left: 3, top: 1}}/>
+                                                </TouchableOpacity>
                                             </View>
-                                        )}/>
-                        </ScrollView>
+                                            <View style={{width: 60}}>
+                                                <Text style={styles.rowText}>{item.system}</Text>
+                                            </View>
+                                            <View style={{width: 75}}>
+                                                <Text style={styles.rowText}>{item.date}</Text>
+                                            </View>
+                                            <View>
+                                                <Text style={styles.rowText}>{item.summary}</Text>
+                                            </View>
+                                        </View>
+                                    )}/>
                     </View>
                 ) : (
                     <View style={{marginTop: 50}}>
@@ -84,7 +111,7 @@ export default function Filter(props) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Jet,
+        backgroundColor: "white",
     },
     listContainer: {
         flex: 1,
@@ -100,16 +127,19 @@ const styles = StyleSheet.create({
         margin: 4,
         borderRadius: 8,
         borderLeftWidth: 3,
-        borderColor: InternationalOrange,
+        borderColor: "black",
     },
     openRecordButton: {
         width: 18,
         margin: 4,
         borderRadius: 5,
-        backgroundColor: Platinum,
+        backgroundColor: "white",
     },
     rowText: {
         fontSize: 16,
-        color: Gainsboro,
+        color: "black",
+    },
+    filterControls: {
+        backgroundColor: "red",
     }
 });
