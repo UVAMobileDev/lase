e2a7dbf2e32607763f1be4b759a100a946281fb5
Sam Kim <samkim@Sams-MacBook-Pro.local>
2020-07-10 00:10:27 -0400

    Working on Classes page

--- components/Landing/subpages/Classes.js
+++ components/Landing/subpages/Classes.js
@@ -7,5 +7,27 @@ export default function Classes(props) {
         <View>
             <Text>Classes</Text>
         </View>
+
+        <View>
+          <Text style={styles.sectionHeader}> EE 383P-6: Optoelectronic Devices (Spring 2007-2015, 2017, 2019, 2021 - Planned)</Text>
+        </View>
+
     );
 }
+
+const styles = StyleSheet.create({
+  container: {backgroundColor: 'white'},
+
+  sectionHeader:{
+    fontWeight: "bold",
+    fontSize: 15,
+    marginLeft: 20,
+    paddingTop: 20,
+    paddingBottom: 10,
+  },
+  }
+
+  // sectionText:{
+  //   fontSize:
+  // }
+});
