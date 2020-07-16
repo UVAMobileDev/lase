283d07abc68f6a8627ec3d28fb6b3ee4074265ea
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-07-16 10:02:34 -0400

    Create starting files for Utilities section

--- components/Utilities/Utilities.js
+++ components/Utilities/Utilities.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+import GrowthCalendar from './GrowthCalendar/GrowthCalendar';
+import STO from './STOGeneration/STOGenerator';
+import WaferLog from './WaferUtils/WaferLog';
+
+const Tab = createMaterialTopTabNavigator();
+
+function Todo(props) {
+    return (<View><Text>This component has not yet been implemented.</Text></View>);
+}
 
 export default function Utilities(props) {
     return (
-        <View>
-            <Text>Future home of the utilities component</Text>
-        </View>
+        <Tab.Navigator  initialRouteName="Browse"
+                        style={{marginTop: Platform.OS === "web" ? 0 : Constants.statusBarHeight,}}
+                        tabBarOptions={{
+                            scrollEnabled: Platform.OS !== "web",
+                        }}>
+            <Tab.Screen name="Growth Calendar" component={GrowthCalendar}/>
+            <Tab.Screen name="STO Generation" component={STO}/>
+            <Tab.Screen name="Wafer Log" component={WaferLog}/>
+            <Tab.Screen name="Delete" component={Todo}/>
+        </Tab.Navigator>
     );
 }
 
