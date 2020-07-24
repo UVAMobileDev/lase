37ccb3f3fa16db76b76ca2148939db3365169e49
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-07-24 14:36:21 -0400

    Remove unused things

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 // UT Austin LASE Database interaction application
 // Designed to support web and native
 
-// Written with IPP Summer 2020 interns!
 // G. Michael Fitzgerald
+
+// Written with IPP Summer 2020 interns!
 // Sam Kim
 // Hien (Taylor) Truong
-//Julia Shea
+// Julia Shea
 
 // Import the necessary components
 import React from 'react';
 import { StyleSheet, Text, View, Platform } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { Jet, InternationalOrange } from './constants/Colors';
-import { Ionicons } from '@expo/vector-icons';
+
 
 // Import our custom screen components.
 import Landing from './components/Landing/Landing.js';
@@ -26,45 +26,13 @@ import Growths from './components/Growths/Growths'
 //  top level navigator.
 const Drawer = createDrawerNavigator();
 
-// Basic screen with nothing but a blurb of text saying the sceen is todo.
-function TodoScreen(props) {
-    return (
-        <View style={styles.container}>
-            <Text>This screen has not been implemented yet.</Text>
-        </View>
-    )
-}
-
-// This function is currently unused, but will hopefully have an eventual purpose
-//  once we figure out how to place icons in the drawer.
-// The reason for a switch is because the name of a drawer item, supplied to the
-//  function here as "name", is unlikely to be the name of the icon we want that
-//  item to have.
-function IconGenerator(name) {
-    switch(name) {
-        default:
-            return ({focused, color, size}) => (<Ionicons color={color} size={size} name={focused ? 'md-home' : 'md-home'}/>)
-    }
-}
-
-// Experimental. Used to style the drawer.
-const drawerStyle = {
-    // backgroundColor: Jet,
-    color: InternationalOrange
-}
-
-// Expiermental. Used to specify certain options to the drawer.
-const drawerOptions = {}
-
 // Top level component for the application.
 export default function App() {
     return (
         <NavigationContainer>
             {/* A NavigationContainer is required to wrap the top level navigator. */}
             <Drawer.Navigator   initialRouteName="Home"
-                                drawerType={Platform.OS === "web" ? "permanent" : "slide"}
-                                contentOptions={drawerOptions}
-                                drawerStyle={drawerStyle}>
+                                drawerType={Platform.OS === "web" ? "permanent" : "slide"}>
                 {/* Each screen has a name to appear in the UI and a component which it displays when selected. */}
                 <Drawer.Screen name="Home" component={Landing}/>
                 <Drawer.Screen name="Growths" component={Growths}/>
