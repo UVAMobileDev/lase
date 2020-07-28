e7b1cb4f2209cd62fc9fd6cdec3d8c4642e65825
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-07-28 16:18:41 -0400

    Remove wrongly added line

--- components/Utilities/Index.js
+++ components/Utilities/Index.js
@@ -6,7 +6,7 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import GrowthCalendar from './GrowthCalendar/GrowthCalendar';
 import STO from './STOGeneration/STOGenerator';
 import WaferLog from './WaferUtils/WaferLog';
-import ExampleComponent from './WaferUtils/ExampleComponent';
+// import ExampleComponent from './WaferUtils/ExampleComponent';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -24,7 +24,7 @@ export default function Utilities(props) {
                             }}>
                 <Tab.Screen name="Growth Calendar" component={GrowthCalendar}/>
                 <Tab.Screen name="STO Generation" component={STO}/>
-                <Tab.Screen name="Wafer Log" component={ExampleComponent}/>
+                <Tab.Screen name="Wafer Log" component={Todo}/>
                 <Tab.Screen name="Delete" component={Todo}/>
             </Tab.Navigator>
         </View>
