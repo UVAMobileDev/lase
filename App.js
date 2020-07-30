e85c9f4baecab559c81175f3a92cc683ef934fcb
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-07-29 23:12:48 -0400

    Enter/store key without secret file; add settings panel to enter key

--- App.js
+++ App.js
@@ -9,10 +9,13 @@
 // Julia Shea
 
 // Import the necessary components
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Platform, TouchableOpacity } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { AntDesign } from '@expo/vector-icons';
+
 
 // Create left side drawer navigator. For this application, this drawer is the
 //  top level navigator.
@@ -29,37 +32,55 @@ const Screens = [
 
 // Top level component for the application.
 export default function App() {
-    const [privileged, setPriviledged] = useState(true);
+    const [privileged, setPriviledged] = useState(false);
+    const [key, setKey] = useState("");
+
+    const update = ({verified, key}) => {
+        setPriviledged(verified);
+        setKey(key);
+    }
+
+    const ScreenParams = {
+        Settings: {update}
+    }
+
+    useEffect(() => {
+        const read = async () => {
+            try {
+                let key = await AsyncStorage.getItem('lase-api-key');
+                key = key ? JSON.parse(key) : null;
+                if(key && key.verified) {
+                    setPriviledged(true);
+                    setKey(key.key);
+                }
+            } catch(e) { console.log("Failed to load stored key from AsyncStorage") }
+        }
+        read();
+    }, []);
 
     return (
-        <NavigationContainer>
+        <NavigationContainer style={{flex: 1}}>
             {/* A NavigationContainer is required to wrap the top level navigator. */}
-            <Drawer.Navigator   initialRouteName="Home"
-                                drawerType={Platform.OS === "web" ? "permanent" : "slide"}>
+            <Drawer.Navigator initialRouteName="Home"
+                        drawerType={Platform.OS === "web" ? "permanent" : "slide"}
+                        drawerStyle={{backgroundColor: "#0AA"}}
+                        drawerContentOptions={{
+                            activeTintColor: "#fff",
+                            inactiveTintColor: "#000",
+                        }}>
                 {/* Each screen has a name to appear in the UI and a component which it displays when selected. */}
                 {
                     Screens.filter(screen => privileged ? true : !screen.privileged)
                     .map((screen, i) => (
                         <Drawer.Screen key={i}
                                 name={screen.name}
-                                component={screen.component}/>
+                                component={screen.component}
+                                initialParams={ScreenParams[screen.name] || {}}/>
                     ))
                 }
             </Drawer.Navigator>
         </NavigationContainer>
     );
-
-    // <Drawer.Screen name="Home" component={Landing}/>
-    // {
-    //     /* Only display priviledged screens if valid api key is present */
-    //     privileged ? (
-    //         <Drawer.Screen name="Growths" component={Growths}/>
-    //     ) : (<View/>)
-    // }
-    // <Drawer.Screen name="Maintenance" component={Maintenance}/>
-    // <Drawer.Screen name="Publications" component={Public}/>
-    // <Drawer.Screen name="Utilities" component={Utilities}/>
-    // <Drawer.Screen name="Settings" component={Settings} initialParams={{setPriviledged}}/>
 }
 
 // Create the stylesheet for the root component
--- components/Settings.js
+++ components/Settings.js
@@ -1,17 +1,143 @@
-import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import Constants from 'expo-constants';
+import { StyleSheet, Text, View, Platform, TouchableOpacity, TextInput, ActivityIndicator } from 'react-native';
+import { BASE_URL } from '../constants/API';
+import { API_KEY } from '../keys.js';
+
+// VALID KEY
+// 2yRALZXus73AaKg5c1wWv7qf4DTSx84naQGIGCCf
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+const AttemptActivation = async (key, update, setVerifying, setKey) => {
+    setVerifying(true);
+    let verification = await fetch(`${BASE_URL}/verify`, {
+        method: "GET",
+        headers: { "x-api-key": key }
+    }).catch(() => false);
+
+    let verified = verification && verification.status === 200;
+    let store = {verified, key};
+
+    try {
+        await AsyncStorage.setItem('lase-api-key', JSON.stringify(store));
+    } catch(e) { console.log(e); }
+
+    update(store);
+    setKey(store);
+    setVerifying(false);
+}
+
+const DestroyToken = async () => {
+    try {
+        await AsyncStorage.removeItem('lase-api-key');
+    } catch(e) { console.log(e); }
+}
 
 export default function Settings(props) {
+    const [verifying, setVerifying] = useState(false);
+    const [key, setKey] = useState({});
+
+    // On page load, the root component will attempt to preload the key from
+    // storage and look for previous verification. Since this side effect won't
+    // run until the Settings tab is selected, we don't need to worry about propagating
+    // the loaded key back up the hierarchy on the "initial" loading.
+    useEffect(() => {
+        const read = async () => {
+            try {
+                let key = await AsyncStorage.getItem('lase-api-key');
+                if(key) setKey(JSON.parse(key));
+            } catch(e) { console.log("Failed to load stored key from AsyncStorage") }
+        }
+        read();
+    }, []);
 
     return (
-        <View>
-            <Text>Settings</Text>
+        <View style={{flex: 1, backgroundColor: "#0AA"}}>
+            <View style={styles.container}>
+                <View style={[styles.section, {borderTopWidth: 0}]}>
+                    <Text style={styles.sectionTitle}>Access Key</Text>
+                    <Text style={styles.sectionDescription}>Entering a valid access key will reveal additional features.</Text>
+                    <View style={{flexDirection: "row", alignItems: "center"}}>
+                        <Text style={styles.inputLabel}>Key: </Text>
+                        <TextInput style={styles.input}
+                                value={key.key}
+                                onChangeText={val => setKey({verified: key.verified, key: val})}/>
+                    </View>
+
+                    <View style={styles.buttonRow}>
+                        {
+                            verifying ? (
+                                <View style={styles.button}>
+                                    <ActivityIndicator />
+                                </View>
+                            ) : (
+                                <TouchableOpacity style={styles.button}
+                                        onPress={() => AttemptActivation(key.key, props.route.params.update, setVerifying, setKey)}>
+                                    <Text style={{textAlign: "center"}}>Update Key</Text>
+                                </TouchableOpacity>
+                            )
+                        }
+                        <View style={[styles.button, key.verified ? {} : {backgroundColor: "red"}]}>
+                            <Text style={{textAlign: "center"}}>{key.verified ? "Key has been verified!" : "Unverified key."}</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.sectionTitle}>Light and Dark Mode</Text>
+                    <Text style={styles.sectionDescription}>Are your eyes on fire from a bright white user experience? Or maybe you're having trouble reading anything with a dark color palette in a bright working environment. Here you can switch between light and dark palettes.</Text>
+                    <Text>Eventually a toggle switch will be here.</Text>
+                </View>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    buttonRow: {
+        flexDirection: Platform.OS === "web" ? "row" : "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        maxWidth: 400,
+    },
+    input: {
+        width: "90%",
+        marginVertical: 5,
+        borderBottomWidth: .5,
+        borderColor: "#999",
+    },
+    inputLabel: {
+        fontWeight: "bold",
+    },
+    sectionDescription: {
+        fontSize: 15,
+        marginLeft: 5,
+        marginBottom: 10,
+    },
+    sectionTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    section: {
+        padding: 15,
+        borderColor: "black",
+        borderTopWidth: 2,
+    },
+    button: {
+        margin: 10,
+        padding: 10,
+        borderRadius: 5,
+        borderColor: "black",
+        borderWidth: 2,
+        width: Platform.OS === "web" ? "40%" : "80%",
+        height: 45,
+        justifyContent: "center",
+        backgroundColor: "#53ff23",
+    },
     container: {
-
+        flex: 1,
+        backgroundColor: "white",
+        marginTop: Platform.OS === "web" ? 0 : Constants.statusBarHeight,
     }
 });
