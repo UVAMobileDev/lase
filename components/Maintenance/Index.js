bc7f0d51998f9496a3b0f616e7dfaf4d8b1f3126
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-08-20 10:33:56 -0400

    Remove excess "modularization"

--- components/Maintenance/Index.js
+++ components/Maintenance/Index.js
@@ -6,13 +6,30 @@ import React from 'react';
 import Constants from 'expo-constants';
 import { View, Text, Platform } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import Browse from './Find/Browse';
+import { createStackNavigator } from '@react-navigation/stack';
+import Record from './Find/Record.js';
+import Filter from './Find/Filter';
 import Insert from './Insert/Insert';
 
 const onWeb = Platform.OS === "web";
 
 // Create the tab navigator which separates the Browse and Create sections.
 const Tab = createMaterialTopTabNavigator();
+
+const BrowseStack = createStackNavigator();
+
+// Browse is the component which appears under the Maintenance/Find tab. Since
+//  it allows you to look at the list of records in the database, as well as an
+//  individual record, the top level component here just creates a stack navigator
+//  to wrap the list/filter and creation functionalities.
+function Browse(props) {
+    return (
+        <BrowseStack.Navigator>
+            <BrowseStack.Screen name="Filter" component={Filter} options={{headerShown: false}}/>
+            <BrowseStack.Screen name="Record" component={Record}/>
+        </BrowseStack.Navigator>
+    );
+}
 
 export default function Maintenance(props) {
     return (
